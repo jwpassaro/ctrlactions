2bbb15c706078c3edc8a28b83873eca031d8545d
Jennifer Passaro <jennifer.wheatley@gmail.com>
2015-02-02 20:22:59 -0500

    Trying to save session texts

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -2,15 +2,33 @@ class MeController < ApplicationController
 
   def show
 
+    if session[:messages].present?
+      @messages = session[:messages]
+    else
+      @messages = []
+    end
+
     #call the method of send a message
 
     #send_awesome_text_message(params[:phone], params[:message])
+    if params[:message].present?
 
-    puts "sent message #{params[:message]} to #{params[:phone]}"
+      puts "sent message #{params[:message]} to #{params[:phone]}"
+      if session[:message_count].present?
+        session[:message_count] = session[:message_count] + 1
+      else
+        session[:message_count] = 1
+      end
+      @messages.push(params[:message])
 
-    #render(text: "yo")
+    end
 
+    #session[:messages] = @messages
     @message = params[:message]
 
+    @message_count = session[:message_count]
+
+    end
   end
-end
+
+#byebug
